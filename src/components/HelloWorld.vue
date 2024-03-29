6d0ca97c61b10a2553c4efba7a2f81166c4054e5
Lucrecia Aloi <lumad29@gmail.com>
2024-03-29 14:03:19 -0300

    added checkbox, remove btn, select all func

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,50 +1,16 @@
-<template>
-  <v-container class="fill-height">
-    <v-row justify="center" class="bg-bluex">
-
-
-      <v-col cols="6" class="bg-redx">
-    <v-responsive class="align-center text-center fill-height">
-      <!-- <v-img height="60" src="@/assets/logo.svg" /> -->
-
-      <h1 class="text-h6 font-weight-light">Shopping List</h1>
-
-      <v-text-field label="Shop" variant="underlined" v-model="newItem" @keyup.enter="addItem" />
-
-      <div v-for="(item, index) in items" :key="index">
-        <div class="ma-3" >
-         <v-card class="bg-teal-lighten-2 pa-1 d-flex justify-end" >
-          {{ item.title }} 
-           <v-btn density="compact" class="lowercase ml-16" @click="removeItem(index)">delete</v-btn> 
-         </v-card>
-       </div>
-      </div>
-
-      <div class="py-14" />
-
-    </v-responsive>
-  </v-col>
-
-  <v-btn density="compact" icon="mdi-plus" class="bg-teal-lighten-1 mt-16" @click="addItem"/>
-
-  </v-row>
-  </v-container>
-</template>
-
-<script setup> 
-
-
-import {ref} from 'vue'
+<script setup>
+import { ref } from 'vue'
 
 const items = ref([])
 const newItem = ref('')
 
 function addItem() {
-  if (newItem.value.trim() === "") {
-    return;
-  }
+  if (newItem.value.trim() === '')
+    return
+
   items.value.push({
-    title: newItem.value
+    title: newItem.value,
+    isChecked: false,
   })
   newItem.value = ''
 }
@@ -53,7 +19,15 @@ function removeItem(index) {
   items.value.splice(index, 1)
 }
 
-// - - -  object API - - - 
+function removeAllItems() {
+  items.value = []
+}
+
+function toggleAllItems() {
+  const allItemsSelected = items.value.every(item => item.isChecked)
+  items.value.forEach((item) => { item.isChecked = !allItemsSelected })
+}
+// - - -  object API - - -
 
 // export default {
 //     name: "todo-list",
@@ -70,24 +44,89 @@ function removeItem(index) {
 //             }
 //             this.items.push({
 //                 id: this.idTodo,
-//                 title: this.newItem, 
+//                 title: this.newItem,
 //             })
 //             this.newItem = '',
 //             this.idTodo++
-//         }, 
+//         },
 //         removeTodo(index) {
 //            this.items.splice(index, 1)
 //         }
 //     }
 // }
 
-// - - -  Composition API - - - 
-
-
+// - - -  Composition API - - -
 </script>
 
+<template>
+  <v-container class="fill-height">
+    <v-row justify="center" class="bg-bluex">
+      <v-col cols="6" class="bg-redx">
+        <v-responsive class="align-center text-center fill-height">
+          <!-- <v-img height="60" src="@/assets/logo.svg" /> -->
+
+          <h1 class="text-h6 font-weight-light">
+            Shopping List
+          </h1>
+
+          <v-text-field v-model="newItem" label="Add product" variant="underlined" @keyup.enter="addItem">
+            <template #append-inner>
+              <v-btn density="compact" icon="mdi-plus" class="bg-teal-lighten-1" @click="addItem" />
+            </template>
+          </v-text-field>
+
+          <v-card v-if="items.length > 0" class="pa-1 pl-8 d-flex align-center justify-end" flat>
+            <div>
+              <v-btn
+                title="remove all products" variant="plain"
+                density="compact"
+                class="lowercase ml-4 pr-8"
+                @click="removeAllItems"
+              >
+                Remove all
+              </v-btn>
+            </div>
+
+            <v-checkbox title="check all products" density="compact" variant="plain" class="d-flex" @click="toggleAllItems" />
+          </v-card>
+
+          <div v-for="(item, index) in items" :key="index">
+            <div class="my-2">
+              <v-card class="bg-teal-lighten-2 pa-1 pl-8 d-flex align-center">
+                <div class="spacer d-flex align-center" :class="{ 'item-checked': item.isChecked }">
+                  {{ item.title }}
+                  <v-divider v-if="item.isChecked" :thickness="2" color="white" class="border-opacity-75" />
+                </div>
+
+                <div>
+                  <v-btn
+                    title="remove product" variant="plain" icon="mdi-trash-can" density="compact"
+                    class="lowercase ml-4 pr-8" @click="removeItem(index)"
+                  />
+                </div>
+                <v-checkbox v-model="item.isChecked" title="check product" density="compact" variant="plain" class="d-flex" />
+              </v-card>
+            </div>
+          </div>
+
+          <div class="py-14" />
+        </v-responsive>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
 <style scoped>
-.v-btn.lowercase{
-    text-transform: unset;
+.v-btn.lowercase {
+  text-transform: unset;
+}
+.item-checked {
+  text-decoration: line-through;
+}
+.spacer {
+  width: 100%;
+}
+input.item-checked {
+  color: whitesmoke
 }
 </style>
